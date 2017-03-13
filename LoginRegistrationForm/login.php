3b7e8303ff5f967133b29c2116b069419dee4cec
Alex Tai <alexjasontai@gmail.com>
2017-03-13 23:10:41 +0800

    login register user databse + journal

--- LoginRegistrationForm/login.php
+++ LoginRegistrationForm/login.php
@@ -3,5 +3,18 @@
 require ("config.php");
 $connection = pg_connect ("host=$DB_HOST dbname=$DB_DATABASE user=$DB_USER password=$DB_PASSWORD");
 
+// Check connection
+if (!$connection){
+    echo "Connection error";
+}
+
+$name = $_POST['username'];
+$pass = $_POST['password'];
+
+$query = "SELECT * FROM users WHERE username='$name' AND password=crypt('$pass', password);"
+
+$result = pg_fetch_array(pg_query($query));
+
+echo "The result is".$result[0]
 
 ?>
